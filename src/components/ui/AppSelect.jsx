6ff59658ad14acd0c5da417ca3e45c684597d136
Julienne Ybañez <dragonsandprincess@gmail.com>
2026-09-01 10:20:29 +0800

    ui: add AppSelect primitive

---
+++ src/components/ui/AppSelect.jsx
@@ -0,0 +1,15 @@
+import { forwardRef } from 'react';
+
+const AppSelect = forwardRef(function AppSelect({ className = '', children, ...props }, ref) {
+  return (
+    <select
+      ref={ref}
+      className={['st-form-control', 'st-select', className].filter(Boolean).join(' ')}
+      {...props}
+    >
+      {children}
+    </select>
+  );
+});
+
+export default AppSelect;
