0460b8e94ac3815b3dc792bef0acc1826b3cf3a7
Julienne Ybañez <dragonsandprincess@gmail.com>
2026-09-01 17:39:12 +0800

    fix: satisfy lint in mockup primitives

--- src/components/ui/SoftPanel.jsx
+++ src/components/ui/SoftPanel.jsx
@@ -1,7 +1,9 @@
-export default function SoftPanel({ as: Component = 'section', className = '', children, ...props }) {
-  return (
-    <Component className={['mock-soft-panel', className].filter(Boolean).join(' ')} {...props}>
-      {children}
-    </Component>
+import { createElement } from 'react';
+
+export default function SoftPanel({ as: component = 'section', className = '', children, ...props }) {
+  return createElement(
+    component,
+    { className: ['mock-soft-panel', className].filter(Boolean).join(' '), ...props },
+    children,
   );
 }
