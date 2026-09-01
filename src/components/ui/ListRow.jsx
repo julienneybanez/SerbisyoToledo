93e05915d67714f4a2086a704edaea66b55be331
Julienne Ybañez <dragonsandprincess@gmail.com>
2026-09-01 17:39:20 +0800

    fix: satisfy lint in mockup primitives

--- src/components/ui/ListRow.jsx
+++ src/components/ui/ListRow.jsx
@@ -1,7 +1,9 @@
-export default function ListRow({ as: Component = 'div', className = '', children, ...props }) {
-  return (
-    <Component className={['mock-listrow', className].filter(Boolean).join(' ')} {...props}>
-      {children}
-    </Component>
+import { createElement } from 'react';
+
+export default function ListRow({ as: component = 'div', className = '', children, ...props }) {
+  return createElement(
+    component,
+    { className: ['mock-listrow', className].filter(Boolean).join(' '), ...props },
+    children,
   );
 }
